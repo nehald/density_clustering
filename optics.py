8f2b68ff398b40a7a4405547766e82723738c221
nehalmeister@gmail.com <nehalmeister@gmail.com>
2014-12-21 07:59:38 +0000

    new

--- optics.py
+++ optics.py
@@ -6,9 +6,11 @@ import get_data
 import AutomaticClustering as AutoC
 import matplotlib.pyplot as plt
 import f
+
+
 def haversine(A, B):
-    """ This function Calculates the distance between points A and B.
-    A and B are tuples.. A= (longitude,latitude) """
+    """	Args:  A and B are Lon/Lat point.  Returns distance between
+	A and B in kilometers """	
     lon1, lat1 = A
     lon2, lat2 = B
     R = 6372.8  # Earth radius in kilometers
@@ -22,25 +24,25 @@ def haversine(A, B):
 
 
 def update(core_dist_pt, distance_mat, point, seeds):
-    ## find all the neighbors
+    # find all the neighbors
     cd_vector = np.ones(len(seeds)) * core_dist_pt
     reach_vector = distance_mat[point][seeds]
     temp = np.column_stack((cd_vector, reach_vector))
-    #pdb.set_trace()
-    maxtemp =  np.max(temp, axis=1)
+    # pdb.set_trace()
+    maxtemp = np.max(temp, axis=1)
     return maxtemp
 
 
-
 def dist_mat(points, metric):
-	if metric == 'haversine':
-    		distance_mat = spatial.distance.pdist(points, lambda u, v: haversine(u, v))
-   	else:
-    		distance_mat = spatial.distance.pdist(points, metric)
-	return spatial.distance.squareform(distance_mat)
+    if metric == 'haversine':
+        distance_mat = spatial.distance.pdist(
+            points, lambda u, v: haversine(u, v))
+    else:
+        distance_mat = spatial.distance.pdist(points, metric)
+    return spatial.distance.squareform(distance_mat)
 
 
-def optics(points, min_cluster_size,metric):
+def optics(points, min_cluster_size, metric):
     # setup variables
     """ The optics algorithm.  The optics algorithm tries overcome the
         limitation of the DBSCAN algorithms.  The main limitation of the DBSCAB
@@ -50,95 +52,92 @@ def optics(points, min_cluster_size,metric):
     m, n = points.shape
     rd = np.ones(m) * 100000000
 #    cd = {i: -1 for i in range(m)}
-    cd = np.ones(m)*-1;    
+    cd = np.ones(m) * -1
     ordered = []
-    
+
     try:
-	distance_mat = np.load("dist_mat.npy")
+        distance_mat = np.load("dist_mat.npy")
     except:
         print 'Creating new dist_mat'
-    	distance_mat = dist_mat(np.array(X), metric)
-        np.save("dist_mat",distance_mat)
+        distance_mat = dist_mat(np.array(X), metric)
+        np.save("dist_mat", distance_mat)
     tmp = np.zeros((m, m)) - 1
-    # calculate core distance.  The core distance 
-    # is the distance from a point to its nth-neighbor 
-    #  
+    # calculate core distance.  The core distance
+    # is the distance from a point to its nth-neighbor
+    #
     for point in xrange(m):
         try:
-	    ## get neighbor list in sorted order (closest to farthest)
+            # get neighbor list in sorted order (closest to farthest)
             nbr_list = sorted([i for i in distance_mat[point] if i > 0])
             if len(nbr_list) > min_cluster_size - 1:
                 cd[point] = nbr_list[min_cluster_size - 1]
-	except:
-            	cd[point] = -1
-	
-    ## calculate the reachability
-    processed=[]
+        except:
+            cd[point] = -1
+
+    # calculate the reachability
+    processed = []
     index = 0
-    seeds =  np.array([i for i in range(0,m)]) 
-    while len(seeds) != 1: 
-	seed_trial = seeds[index] 
-	processed.append(seed_trial)
-	#print processed	
-	seed_indexes = np.where(seeds != seed_trial)
-	seeds = seeds[seed_indexes]
-	## compare the core distance and the reachability		
-	rd_temp=update(cd[seed_trial], distance_mat,seed_trial, seeds)
-	## compare the current reachability matrix with an updated rd
-	## if the updata rd is less then the rd	
-	rd_index = np.where(rd[seeds]>rd_temp)[0]	
-	#pdb.set_trace()	
-	rd[seeds[rd_index]] = rd_temp[rd_index]
-	index = np.argmin(rd[seeds]) 
+    seeds = np.array([i for i in range(0, m)])
+    while len(seeds) != 1:
+        seed_trial = seeds[index]
+        processed.append(seed_trial)
+        # print processed
+        seed_indexes = np.where(seeds != seed_trial)
+        seeds = seeds[seed_indexes]
+        # compare the core distance and the reachability
+        rd_temp = update(cd[seed_trial], distance_mat, seed_trial, seeds)
+        # compare the current reachability matrix with an updated rd
+        # if the updata rd is less then the rd
+        rd_index = np.where(rd[seeds] > rd_temp)[0]
+        # pdb.set_trace()
+        rd[seeds[rd_index]] = rd_temp[rd_index]
+        index = np.argmin(rd[seeds])
     processed.append(seeds[0])
-    rd[0] =0
-    return rd,cd,processed 
-		
+    rd[0] = 0
+    return rd, cd, processed
+
 if __name__ == '__main__':
-	#X = np.load("zhang2.dat.npy")	
-	#X = get_data.get_data()[0:8000]	
-	#rd,cd,processed = optics(np.array(X),30,"euclidean")
- 	X = f.fitz_data()
-	rd,cd,processed = optics(np.array(X),10,"euclidean")
-	# zhang_results= np.array(processed) 
-	# np.save("zhang_results.dat",zhang_results)
-	pdb.set_trace()
-	RPlot = []
-	RPoints = []
-	for item in processed:
-    		RPlot.append(rd[item]) #Reachability Plot
-    		RPoints.append([X[item][0],X[item][1]]) #points in their order determined by OPTICS
-	
-	fig = plt.figure()
-	ax = fig.add_subplot(111)
-	ax.plot(X[:,0], X[:,1], 'b.', ms=2)
-	plt.savefig('Graph.png', dpi=None, facecolor='w', edgecolor='w',
-    		orientation='portrait', papertype=None, format=None,
-		transparent=False, bbox_inches=None, pad_inches=0.1)
-	plt.show()
+        #X = np.load("zhang2.dat.npy")
+    X = np.array(get_data.get_data()[0:7500])
+    #rd,cd,processed = optics(np.array(X),30,"euclidean")
+    #X = f.fitz_data()
+    rd, cd, processed = optics(np.array(X), 150, "haversine")
+    # zhang_results= np.array(processed)
+    # np.save("zhang_results.dat",zhang_results)
+    pdb.set_trace()
+    RPlot = []
+    RPoints = []
+    for item in processed:
+        RPlot.append(rd[item])  # Reachability Plot
+        RPoints.append([X[item][0], X[item][1]])
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    ax.plot(X[:, 0], X[:, 1], 'b.', ms=2)
+    plt.savefig('Graph.png', dpi=None, facecolor='w', edgecolor='w',
+                orientation='portrait', papertype=None, format=None,
+                transparent=False, bbox_inches=None, pad_inches=0.1)
+    plt.show()
 
+    rootNode = AutoC.automaticCluster(RPlot, RPoints)
+    AutoC.graphTree(rootNode, RPlot)
+    # get only the leaves of the tree
+    leaves = AutoC.getLeaves(rootNode, [])
 
-	rootNode = AutoC.automaticCluster(RPlot, RPoints)
-	AutoC.graphTree(rootNode, RPlot)
-	#get only the leaves of the tree
-	leaves = AutoC.getLeaves(rootNode, [])
-
-	fig = plt.figure()
-	ax = fig.add_subplot(111)
-	print X
-  	import itertools	
-        ax.plot(X[:,0], X[:,1], 'y.')
-	colors = itertools.cycle('gmkrcbgrcmk')
-	for item, c in zip(leaves, colors):
-		print item,c
-    		node = []
-    		for v in range(item.start,item.end):
-        		node.append(RPoints[v])
-    		node = np.array(node)
-    		ax.plot(node[:,0],node[:,1], c+'o', ms=5)
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    import itertools
+    ax.plot(X[:, 0], X[:, 1], 'b.', ms=2)
+    colors = itertools.cycle('gmrckwygmrcmkwy')
+    for item, c in zip(leaves, colors):
+        print item, c
+        node = []
+        for v in range(item.start, item.end):
+            node.append(RPoints[v])
+        node = np.array(node)
+        ax.plot(node[:, 0], node[:, 1], c + 'o', ms=2)
 
 plt.savefig('Graph2.png', dpi=None, facecolor='w', edgecolor='w',
-    orientation='portrait', papertype=None, format=None,
-    transparent=False, bbox_inches=None, pad_inches=0.1)
+            orientation='portrait', papertype=None, format=None,
+            transparent=False, bbox_inches=None, pad_inches=0.1)
 plt.show()
-
+ 
